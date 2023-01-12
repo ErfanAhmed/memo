09ae205f7b8064c44950326fd354ccc351358bed
Erfan Ahmed <erfan002@gmail.com>
2023-01-12 15:09:34 +0600

    Bare bone project structure

---
+++ src/main/java/com/example/easymemo/EasymemoApplication.java
@@ -0,0 +1,13 @@
+package com.example.easymemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EasymemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EasymemoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/easymemo/EasymemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.easymemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EasymemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
