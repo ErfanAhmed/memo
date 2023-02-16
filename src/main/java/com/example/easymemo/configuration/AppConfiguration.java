9a807eefd66cc148da135b1213ecf4b0562e29a3
Erfan Ahmed <erfan002@gmail.com>
2023-02-16 12:33:06 +0600

    Added interceptor for i18n Used CookieLocaleResolver

--- src/main/java/com/example/easymemo/configuration/AppConfiguration.java
+++ src/main/java/com/example/easymemo/configuration/AppConfiguration.java
@@ -4,11 +4,13 @@ import org.springframework.context.MessageSource;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.support.ResourceBundleMessageSource;
+import org.springframework.web.servlet.LocaleResolver;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.i18n.CookieLocaleResolver;
+import org.springframework.web.servlet.i18n.LocaleChangeInterceptor;
 
 import java.util.Locale;
-
 /**
  * @author erfan
  * @since 2/13/23
@@ -20,15 +22,22 @@ public class AppConfiguration implements WebMvcConfigurer {
     public MessageSource messageSource() {
         ResourceBundleMessageSource messageSource = new ResourceBundleMessageSource();
         messageSource.setBasename("i18n/messages");
+        // for bangla `new Locale("bn")`
         messageSource.setDefaultLocale(Locale.US);
         messageSource.setDefaultEncoding("UTF-8");
 
         return messageSource;
     }
 
+    @Bean
+    public LocaleResolver localeResolver() {
+        return new CookieLocaleResolver();
+    }
+
     @Override
     public void addInterceptors(InterceptorRegistry registry) {
-        //todo: locale config
-        WebMvcConfigurer.super.addInterceptors(registry);
+        LocaleChangeInterceptor localeChangeInterceptor = new LocaleChangeInterceptor();
+        localeChangeInterceptor.setParamName("lang");
+        registry.addInterceptor(localeChangeInterceptor);
     }
 }
